e3d7b4170f80e0a77efa6e0a3502368cc6f974c1
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-23 17:33:06 +0100

    Define thought routes for CRUD operations and reaction management

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -8,3 +8,19 @@ const {
   addThoughtReaction,
   removeThoughtReaction,
 } = require("../../controllers/thoughtController.js");
+
+// api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addThoughtReaction);
+
+// api/thoughts/:thoughtId/reactions/:reactionId
+router.route("/:thoughtId/reactions/:reactionId").delete(removeThoughtReaction);
