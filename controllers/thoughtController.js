0ec294b4331282630183bbbdc84516c23a6d9599
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-23 17:15:14 +0100

    Add route handler to get a single thought by id

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -9,4 +9,19 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async getSingleThought(req, res) {
+    try {
+      const thought = await Thought.findOne({
+        _id: req.params.thoughtId,
+      }).select("-__v");
+
+      if (!thought) {
+        return res.status(404).json({ message: "No thought with that ID" });
+      }
+
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
