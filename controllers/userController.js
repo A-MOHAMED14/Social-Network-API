6d65f6a4260028ec7bbaba5b723f9d4ae889038e
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-23 16:00:03 +0100

    Add route handler to get a single user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,4 +9,20 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .select("-__v")
+        .populate("Thought")
+        .populate("User");
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
