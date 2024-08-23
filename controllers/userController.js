155b950a16289878531d6541bdb61c322af7af27
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-23 16:02:03 +0100

    Add route handler to delete a user and their associated thoughts

--- controllers/userController.js
+++ controllers/userController.js
@@ -51,4 +51,20 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        return res.status(404).json({ message: "No user with that ID" });
+      }
+
+      await Thought.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({
+        message: "User and associated thoughts deleted!",
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
