9d036957b334c992a7bc1679f0dfbcb7fc9d7181
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-24 14:50:12 +0100

    Remove extra brackets around user data which leads to seeding error

--- utils/data.js
+++ utils/data.js
@@ -1,40 +1,38 @@
 module.exports = [
-  [
-    {
-      username: "rajpatel",
-      email: "raj_patel@gmail.com",
-    },
-    {
-      username: "alicejohnson",
-      email: "alice_johnson@hotmail.com",
-    },
-    {
-      username: "liwei",
-      email: "li_wei@outlook.com",
-    },
-    {
-      username: "omarahmed",
-      email: "omar_ahmed@yahoo.com",
-    },
-    {
-      username: "emilykim",
-      email: "emily_kim@gmail.com",
-    },
-    {
-      username: "idrisali",
-      email: "idris_ali@hotmail.com",
-    },
-    {
-      username: "sophiarodriguez",
-      email: "sophia_rodriguez@outlook.com",
-    },
-    {
-      username: "johndoe",
-      email: "john_doe@yahoo.com",
-    },
-    {
-      username: "carlosgomez",
-      email: "carlos_gomez@gmail.com",
-    },
-  ],
+  {
+    username: "rajpatel",
+    email: "raj_patel@gmail.com",
+  },
+  {
+    username: "alicejohnson",
+    email: "alice_johnson@hotmail.com",
+  },
+  {
+    username: "liwei",
+    email: "li_wei@outlook.com",
+  },
+  {
+    username: "omarahmed",
+    email: "omar_ahmed@yahoo.com",
+  },
+  {
+    username: "emilykim",
+    email: "emily_kim@gmail.com",
+  },
+  {
+    username: "idrisali",
+    email: "idris_ali@hotmail.com",
+  },
+  {
+    username: "sophiarodriguez",
+    email: "sophia_rodriguez@outlook.com",
+  },
+  {
+    username: "johndoe",
+    email: "john_doe@yahoo.com",
+  },
+  {
+    username: "carlosgomez",
+    email: "carlos_gomez@gmail.com",
+  },
 ];
