0044ec04d33fa3693cca8a5c7cbe2b95ada1efdd
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-24 11:14:55 +0100

    Add users data

---
+++ utils/data.js
@@ -0,0 +1,20 @@
+const usersData = [
+  {
+    username: "techguru",
+    email: "techguru@example.com",
+  },
+  {
+    username: "devmaster",
+    email: "devmaster@example.com",
+  },
+  {
+    username: "codewizard",
+    email: "codewizard@example.com",
+  },
+  {
+    username: "webninja",
+    email: "webninja@example.com",
+  },
+];
+
+module.exports = usersData;
