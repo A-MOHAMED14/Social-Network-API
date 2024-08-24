70d601f7a8eaef56fe0b37048217718dc5be1124
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-08-24 11:16:57 +0100

    Add seeding script with error handling, connection check, and user data insertion logic

--- utils/seed.js
+++ utils/seed.js
@@ -1,3 +1,22 @@
 const connection = require("../config/connection.js");
 const User = require("../models/User.js");
 const usersData = require("./data.js");
+
+connection.on("error", (err) => err);
+
+connection.once("open", async () => {
+  console.log("connected ✅");
+
+  let userCheck = await connection.db
+    .listCollections({ name: "users" })
+    .toArray();
+  if (userCheck.length) {
+    await connection.dropCollection("users");
+  }
+
+  await User.collection.insertMany(usersData);
+
+  console.table(usersData);
+  console.info("Seeding complete! 🌱");
+  process.exit(0);
+});
